15582e8220d10a3bf6eb486e154a26fce85f6070
John David Martin <johndavidfive@gmail.com>
2020-01-01 14:59:33 -0700

    starting lti integration

--- server/server.ts
+++ server/server.ts
@@ -1,16 +1,35 @@
-import express from 'express';
 import path from 'path';
+import express from 'express';
+import passport from 'passport';
+import LTIStrategy from 'passport-lti';
+
 
 const PORT = process.env.PORT || 3000;
+
+const strategy = new LTIStrategy({
+    consumerKey: 'testconsumerkey',
+    consumerSecret: 'testconsumersecret'
+    // pass the req object to callback
+    // passReqToCallback: true,
+    // https://github.com/omsmith/ims-lti#nonce-stores
+    // nonceStore: new RedisNonceStore('testconsumerkey', redisClient)
+}, (lti: any, done: any) => {
+    // LTI launch parameters
+    console.dir(lti);
+    // Perform local authentication if necessary
+    return done(null, lti);
+});
+
+passport.use(strategy);
 
 // create server
 const app = express();
 
-// create endpoints to handle LTI Tool Launch Endpoints
-
 // serve static files from the React app
 app.use('/application', express.static(path.join(__dirname + '/../../client/build')));
 
+// create endpoints to handle LTI Tool Launch Endpoints
+app.post('/launch', passport.authenticate('lti'));
 
 // The "catchall" handler: for any request that doesn't match any previous endpoints
 app.get('/application/*', (req, res) => {
